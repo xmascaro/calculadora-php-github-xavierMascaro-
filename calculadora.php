cf39e462dd9f014f2831aa920efd683cd117c4e8
xmascaro <xmascaro@inscamidemar.cat>
2026-03-10 16:10:39 +0100

    Renombrades variable d'entrada y resultat

--- calculadora.php
+++ calculadora.php
@@ -1,21 +1,41 @@
 <?php
-$op = $_POST['op'];
-$n1 = $_POST['n1'];
-$n2 = $_POST['n2'];
+$operacion = $_POST['op'];
+$numero1 = $_POST['n1'];
+$numero2 = $_POST['n2'];
 
-if ($op == "s") {
-    $res = $n1 + $n2;
-} else if ($op == "r") {
-    $res = $n1 - $n2;
-} else if ($op == "m") {
-    $res = $n1 * $n2;
-} else if ($op == "d") {
-    if ($n2 != 0) {
-        $res = $n1 / $n2;
-    } else {
-        $res = "Error";
+function calcular($operacion, $numero1, $numero2) {
+
+    switch ($operacion) {
+
+        case "s":
+            $resultado = $numero1 + $numero2;
+            break;
+
+        case "r":
+            $resultado = $numero1 - $numero2;
+            break;
+
+        case "m":
+            $resultado = $numero1 * $numero2;
+            break;
+
+        case "d":
+            if ($numero2 != 0) {
+                $resultado = $numero1 / $numero2;
+            } else {
+                $resultado = "Error: no es pot dividir per zero";
+            }
+            break;
+
+        default:
+            $resultado = "Error: operació desconeguda";
+            break;
     }
+
+    return $resultado;
 }
 
-echo $res;
+$resultado = calcular($operacion, $numero1, $numero2);
+
+echo $resultado;
 ?>
